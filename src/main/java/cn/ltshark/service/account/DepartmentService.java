6b2e21ff523cf8bbf38e35c56a83ee1cd7129b78
杨凡 <fan.yang@surfront.com>
2015-06-16 11:28:00 +0800

    部门管理

--- src/main/java/cn/ltshark/service/account/DepartmentService.java
+++ src/main/java/cn/ltshark/service/account/DepartmentService.java
@@ -1,9 +1,20 @@
 package cn.ltshark.service.account;
 
+import cn.ltshark.entity.Department;
+import cn.ltshark.repository.DepartmentDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springside.modules.persistence.DynamicSpecifications;
+import org.springside.modules.persistence.SearchFilter;
+
+import java.util.Map;
 
 /**
  * Created by ltshark on 15/6/12.
@@ -14,5 +25,47 @@ public class DepartmentService {
 
     private static Logger logger = LoggerFactory.getLogger(DepartmentService.class);
 
+    private DepartmentDao departmentDao;
 
+    @Autowired
+    public void setDepartmentDao(DepartmentDao departmentDao) {
+        this.departmentDao = departmentDao;
+    }
+
+    public Page<Department> getDepartments(Map<String, Object> searchParams, int pageNumber, int pageSize, String sortType) {
+        PageRequest pageRequest = buildPageRequest(pageNumber, pageSize, sortType);
+        Specification<Department> spec = buildSpecification(searchParams);
+        return departmentDao.findAll(spec, pageRequest);
+    }
+
+    /**
+     * 创建分页请求.
+     */
+    private PageRequest buildPageRequest(int pageNumber, int pagzSize, String sortType) {
+        Sort sort = null;
+        if ("auto".equals(sortType)) {
+            sort = new Sort(Sort.Direction.DESC, "id");
+        } else if ("name".equals(sortType)) {
+            sort = new Sort(Sort.Direction.ASC, "name");
+        }
+
+        return new PageRequest(pageNumber - 1, pagzSize, sort);
+    }
+
+    /**
+     * 创建动态查询条件组合.
+     */
+    private Specification<Department> buildSpecification(Map<String, Object> searchParams) {
+        Map<String, SearchFilter> filters = SearchFilter.parse(searchParams);
+        Specification<Department> spec = DynamicSpecifications.bySearchFilter(filters.values(), Department.class);
+        return spec;
+    }
+
+    public Department getDepartment(Long id) {
+        return departmentDao.findOne(id);
+    }
+
+    public void updateDepartment(Department newDepartment) {
+        departmentDao.save(newDepartment);
+    }
 }
---
+++ src/main/java/cn/ltshark/web/account/DepartmentAdminController.java
@@ -0,0 +1,100 @@
+package cn.ltshark.web.account;
+
+import cn.ltshark.entity.Department;
+import cn.ltshark.service.account.DepartmentService;
+import cn.ltshark.web.task.TaskController;
+import com.google.common.collect.Maps;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springside.modules.web.Servlets;
+
+import javax.servlet.ServletRequest;
+import javax.validation.Valid;
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * Created by surfrong on 2015/6/16.
+ */
+@Controller
+@RequestMapping(value = "/admin/department")
+public class DepartmentAdminController {
+
+    private static Map<String, String> sortTypes = Maps.newLinkedHashMap();
+    static {
+        sortTypes.put("auto", "自动");
+        sortTypes.put("name", "部门名称");
+    }
+    @Autowired
+    private DepartmentService departmentService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String list(@RequestParam(value = "page", defaultValue = "1") int pageNumber,
+                       @RequestParam(value = "page.size", defaultValue = TaskController.PAGE_SIZE) int pageSize,
+                       @RequestParam(value = "sortType", defaultValue = "auto") String sortType, Model model,
+                       ServletRequest request) {
+        Map<String, Object> searchParams = Servlets.getParametersStartingWith(request, "search_");
+
+        Page<Department> Departments = departmentService.getDepartments(searchParams, pageNumber, pageSize, sortType);
+
+        model.addAttribute("Departments", Departments);
+        model.addAttribute("sortType", sortType);
+        model.addAttribute("sortTypes", sortTypes);
+        // 将搜索条件编码成字符串，用于排序，分页的URL
+        model.addAttribute("searchParams", Servlets.encodeParameterStringWithPrefix(searchParams, "search_"));
+
+        return "account/adminDepartmentList";
+    }
+
+//    @RequestMapping(value = "update/{id}", method = RequestMethod.GET)
+//    public String updateForm(@PathVariable("id") Long id, Model model) {
+//        model.addAttribute("Department", departmentService.getDepartment(id));
+//        model.addAttribute("action", "update");
+//        return "account/adminDepartmentForm";
+//    }
+//
+//    @RequestMapping(value = "update", method = RequestMethod.POST)
+//    public String update(@Valid @ModelAttribute("Department") Department Department, RedirectAttributes redirectAttributes) {
+//        departmentService.updateDepartment(Department);
+//        redirectAttributes.addFlashAttribute("message", "更新用户" + Department.getLoginName() + "成功");
+//        return "redirect:/admin/Department";
+//    }
+//
+//    @RequestMapping(value = "delete/{id}")
+//    public String delete(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
+//        Department Department = departmentService.getDepartment(id);
+//        departmentService.deleteDepartment(id);
+//        redirectAttributes.addFlashAttribute("message", "删除用户" + Department.getLoginName() + "成功");
+//        return "redirect:/admin/Department";
+//    }
+
+    @RequestMapping(value = "create", method = RequestMethod.GET)
+    public String createForm(Model model) {
+        model.addAttribute("Department", new Department());
+        model.addAttribute("action", "create");
+        return "account/adminDepartmentForm";
+    }
+
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    public String create(@Valid Department newDepartment, RedirectAttributes redirectAttributes) {
+        newDepartment.setCreateTime(new Date());
+        departmentService.updateDepartment(newDepartment);
+        redirectAttributes.addFlashAttribute("message", "创建部门成功");
+        return "redirect:/admin/Department";
+    }
+
+    /**
+     * 所有RequestMapping方法调用前的Model准备方法, 实现Struts2 Preparable二次部分绑定的效果,先根据form的id从数据库查出Department对象,再把Form提交的内容绑定到该对象上。
+     * 因为仅update()方法的form中有id属性，因此仅在update时实际执行.
+     */
+    @ModelAttribute
+    public void getDepartment(@RequestParam(value = "id", defaultValue = "-1") Long id, Model model) {
+        if (id != -1) {
+            model.addAttribute("Department", departmentService.getDepartment(id));
+        }
+    }
+}
