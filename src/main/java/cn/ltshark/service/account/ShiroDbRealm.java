70ad480403502db5113662b547186284f01546db
杨凡 <fan.yang@surfront.com>
2015-06-18 19:22:36 +0800

    增加LDAP认证

--- src/main/java/cn/ltshark/service/account/ShiroDbRealm.java
+++ src/main/java/cn/ltshark/service/account/ShiroDbRealm.java
@@ -5,29 +5,42 @@
  *******************************************************************************/
 package cn.ltshark.service.account;
 
-import java.io.Serializable;
-
-import javax.annotation.PostConstruct;
-
+import cn.ltshark.entity.User;
 import cn.ltshark.shiro.CaptchaException;
 import cn.ltshark.shiro.UsernamePasswordCaptchaToken;
 import cn.ltshark.web.account.VerifyCodeServlet;
+import com.google.common.base.Objects;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
-import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.ldap.UnsupportedAuthenticationMechanismException;
+import org.apache.shiro.realm.ldap.JndiLdapRealm;
+import org.apache.shiro.realm.ldap.LdapContextFactory;
+import org.apache.shiro.realm.ldap.LdapUtils;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
-import cn.ltshark.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springside.modules.utils.Encodes;
 
-import com.google.common.base.Objects;
+import javax.annotation.PostConstruct;
+import javax.naming.AuthenticationNotSupportedException;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.naming.ldap.LdapContext;
+import java.io.Serializable;
 
-public class ShiroDbRealm extends AuthorizingRealm {
+public class ShiroDbRealm extends JndiLdapRealm {
+
+    private static final Logger log = LoggerFactory.getLogger(ShiroDbRealm.class);
 
     protected AccountService accountService;
+    protected String rootDN;
+    private boolean enableLDAP = false;
 
     /**
      * 认证回调函数,登录时调用.
@@ -49,6 +62,30 @@ public class ShiroDbRealm extends AuthorizingRealm {
             return null;
         }
 
+        if (enableLDAP) {
+            AuthenticationInfo info;
+            try {
+                info = queryForAuthenticationInfo(token, getContextFactory());
+            } catch (AuthenticationNotSupportedException e) {
+                String msg = "Unsupported configured authentication mechanism";
+                throw new UnsupportedAuthenticationMechanismException(msg, e);
+            } catch (javax.naming.AuthenticationException e) {
+                String msg = "LDAP authentication failed.";
+                throw new AuthenticationException(msg, e);
+            } catch (NamingException e) {
+                String msg = "LDAP naming error while attempting to authenticate user.";
+                throw new AuthenticationException(msg, e);
+            } catch (UnknownAccountException e) {
+                String msg = "UnknownAccountException";
+                throw new UnknownAccountException(msg, e);
+            } catch (IncorrectCredentialsException e) {
+                String msg = "IncorrectCredentialsException";
+                throw new IncorrectCredentialsException(msg, e);
+            }
+            if (info == null) {
+                return null;
+            }
+        }
         byte[] salt = Encodes.decodeHex(user.getSalt());
         return new SimpleAuthenticationInfo(new ShiroUser(user.getId(), user.getLoginName(), user.getName()),
                 user.getPassword(), ByteSource.Util.bytes(salt), getName());
@@ -67,6 +104,38 @@ public class ShiroDbRealm extends AuthorizingRealm {
         return info;
     }
 
+    @Override
+    protected AuthenticationInfo queryForAuthenticationInfo(AuthenticationToken token, LdapContextFactory ldapContextFactory) throws NamingException {
+        Object principal = token.getPrincipal();
+        Object credentials = token.getCredentials();
+        LdapContext systemCtx = null;
+        LdapContext ctx = null;
+        try {
+            systemCtx = ldapContextFactory.getSystemLdapContext();
+            SearchControls constraints = new SearchControls();
+            constraints.setSearchScope(SearchControls.SUBTREE_SCOPE);
+            NamingEnumeration results = systemCtx.search(rootDN, "cn=" + principal, constraints);
+            if (results != null && !results.hasMore()) {
+                throw new UnknownAccountException();
+            } else {
+                while (results.hasMore()) {
+                    SearchResult si = (SearchResult) results.next();
+                    principal = si.getName() + "," + rootDN;
+                }
+                log.info("DN=" + principal);
+                try {
+                    ctx = ldapContextFactory.getLdapContext(principal, credentials);
+                } catch (NamingException e) {
+                    throw new IncorrectCredentialsException();
+                }
+                return createAuthenticationInfo(token, principal, credentials, ctx);
+            }
+        } finally {
+            LdapUtils.closeContext(systemCtx);
+            LdapUtils.closeContext(ctx);
+        }
+    }
+
     /**
      * 设定Password校验的Hash算法与迭代次数.
      */
@@ -80,6 +149,14 @@ public class ShiroDbRealm extends AuthorizingRealm {
 
     public void setAccountService(AccountService accountService) {
         this.accountService = accountService;
+    }
+
+    public void setRootDN(String rootDN) {
+        this.rootDN = rootDN;
+    }
+
+    public void setEnableLDAP(boolean enableLDAP) {
+        this.enableLDAP = enableLDAP;
     }
 
     /**
