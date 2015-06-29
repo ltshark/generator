ec5d5bb6fcb292b5cad7b91f9a4e9c7ea72335f6
fan.yang <fan.yang@surfront.com>
2015-06-30 01:11:57 +0800

    增加springldap

---
+++ src/main/java/cn/ltshark/domain/DirectoryType.java
@@ -0,0 +1,8 @@
+package cn.ltshark.domain;
+
+/**
+ * @author Mattias Hellborg Arthursson
+ */
+public enum DirectoryType {
+    NORMAL, AD;
+}
---
+++ src/main/java/cn/ltshark/domain/impl/LdapTest.java
@@ -0,0 +1,48 @@
+package cn.ltshark.domain.impl;
+
+/**
+ * Created by ltshark on 15/6/30.
+ */
+import java.util.Properties;
+
+import javax.naming.Context;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.naming.ldap.InitialLdapContext;
+import javax.naming.ldap.LdapContext;
+
+public class LdapTest {
+    public static void main(String[] args) {
+        Properties env = new Properties();
+        String adminName = "administrator@yaic.com.cn";//username@domain
+        String adminPassword = "Yf821010";//password
+        String ldapURL = "LDAP://172.16.104.156:389";//ip:port
+        env.put(Context.INITIAL_CONTEXT_FACTORY,"com.sun.jndi.ldap.LdapCtxFactory");
+//        env.put(Context.SECURITY_AUTHENTICATION, "simple");//"none","simple","strong"
+        env.put(Context.SECURITY_PRINCIPAL, adminName);
+        env.put(Context.SECURITY_CREDENTIALS, adminPassword);
+        env.put(Context.PROVIDER_URL, ldapURL);
+        try {
+            LdapContext ctx = new InitialLdapContext(env, null);
+            SearchControls searchCtls = new SearchControls();
+            searchCtls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+            String searchFilter = "(&(objectCategory=person)(objectClass=user)(name=*))";
+            String searchBase = "DC=yaic,DC=com,DC=cn";
+            String returnedAtts[] = {"memberOf"};
+            searchCtls.setReturningAttributes(returnedAtts);
+            NamingEnumeration<SearchResult> answer = ctx.search(searchBase, searchFilter,searchCtls);
+            while (answer.hasMoreElements()) {
+                SearchResult sr = (SearchResult) answer.next();
+                System.out.println("<<<::[" + sr.getName()+"]::>>>>");
+                System.out.println("<<<::[" + sr.getAttributes()+"]::>>>>");
+                System.out.println("<<<::[" + sr.getNameInNamespace()+"]::>>>>");
+            }
+            ctx.close();
+        }catch (NamingException e) {
+            e.printStackTrace();
+            System.err.println("Problem searching directory: " + e);
+        }
+    }
+}
